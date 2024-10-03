560013473afb8ed6da64369ea179ff5e9ab0e575
Sricharan Reddy Varra <sricharanvarra@gmail.com>
2024-10-03 11:36:01 -0700

    🧐 Initial Scores for NBL Cells
    Initial scoring

--- src/nbl/pp/basic.py
+++ src/nbl/pp/basic.py
@@ -1,18 +1,22 @@
+from collections.abc import Mapping, Sequence
+from typing import Any, Literal, TypedDict, Unpack
+
 import anndata as ad
 import numpy as np
+import scanpy as sc
 import spatialdata as sd
-from numpy.typing import NDArray
+from numpydantic import NDArray
 from scipy import sparse
 
-from nbl.util import write_elements
+from nbl.util import _extract_layer_from_sdata, write_elements
 
 
 def arcsinh_transform(
     sdata: sd.SpatialData,
     table_names: str | list[str] = None,
-    shift_factor: np.integer | np.floating = 0,
-    scale_factor: np.integer | np.floating = 150,
-    replace_X: bool = False,
+    shift_factor: int | float | Mapping[str, int | float] = 0,
+    scale_factor: int | float | Mapping[str, int | float] = 150,
+    method: Literal["replace", "layer", "new table"] = "new table",
     write: bool = True,
     inplace: bool = True,
 ) -> None | sd.SpatialData:
@@ -20,25 +24,24 @@ def arcsinh_transform(
 
     Parameters
     ----------
-    sdata : sd.SpatialData
+    sdata
         The SpatialData object containing the data.
-    table_names : str | list[str], optional
+    table_names
         The name(s) of the table(s) to transform. If None, all tables will be transformed, by default None.
-    shift_factor : np.integer | np.floating, optional
+    shift_factor
         The shift factor to be added to the data before applying the transformation, by default 0.
-    scale_factor : np.integer | np.floating, optional
+    scale_factor
         The scale factor to be multiplied with the data after applying the transformation, by default 150.
-    replace_X : bool, optional
-        Whether to replace the original data in `AnnData.X` with the transformed data, by default False.
-    write : bool, optional
+    method
+        Whether to replace the original in `AnnData.X` with the transformed data, to add it as a new layer, or to create a new table, by default "new table".
+    write
         Whether to write the transformed tables back to the SpatialData object's Zarr store, by default True.
-    inplace : bool, optional
+    inplace
         Whether to modify the SpatialData object in-place or return a new object, by default True.
 
     Returns
     -------
-    None | sd.SpatialData
-        If inplace is `True`, returns `None`. If inplace is `False`, returns the modified `SpatialData` object.
+    If inplace is `True`, returns `None`. If inplace is `False`, returns the modified `SpatialData` object.
 
 
     Notes
@@ -62,15 +65,150 @@ def arcsinh_transform(
     for table in table_keys:
         adata: ad.AnnData = sdata.tables[table]
         if sparse.issparse(adata.X):
-            transformed_X: NDArray = sparse.csr_matrix(np.arcsinh(shift_factor + (adata.X.toarray() * scale_factor)))
+            transformed_X: NDArray = np.arcsinh(shift_factor + (adata.X.toarray() * scale_factor))
         else:
-            transformed_X: NDArray = sparse.csr_matrix(np.arcsinh(shift_factor + (adata.X * scale_factor)))
+            transformed_X: NDArray = np.arcsinh(shift_factor + (adata.X * scale_factor))
 
-        if replace_X:
+        if method == "replace":
             adata.X = transformed_X
-        else:
+        elif method == "layer":
             adata.layers[f"arcsinh_shift_{shift_factor}_scale_{scale_factor}"] = transformed_X
+        elif method == "new table":
+            new_table: ad.AnnData = adata.copy()
+            new_table.X = transformed_X
+            sdata.tables[f"arcsinh_shift_{shift_factor}_scale_{scale_factor}"] = new_table
 
     if write:
-        write_elements(sdata=sdata, elements={"tables": table_keys})
+        if method in ["replace", "layer"]:
+            write_elements(sdata=sdata, elements={"tables": table_keys})
+        elif method == "new table":
+            write_elements(sdata=sdata, elements={"tables": [f"arcsinh_shift_{shift_factor}_scale_{scale_factor}"]})
+    return None if inplace else sdata
+
+
+def normalize_by_area(
+    sdata: sd.SpatialData,
+    table_names: str | list[str] = None,
+    method: Literal["replace", "layer", "new table"] = "new table",
+    inplace: bool = True,
+    write: bool = True,
+):
+    """Normalize the data by the area of each cell.
+
+    Parameters
+    ----------
+    sdata
+        The SpatialData object.
+    table_names
+        The name(s) of the table(s) to transform. If None, all tables will be transformed, by default None.
+    inplace
+        Whether to modify the SpatialData object in-place or return a new object, by default True.
+    write
+        Whether to write the transformed tables back to the SpatialData object's Zarr store, by default True.
+
+    Returns
+    -------
+    The updated SpatialData object if inplace is False, otherwise None.
+    """
+    table_keys = [table_names] if isinstance(table_names, str) else table_names
+
+    for table in table_keys:
+        adata: ad.AnnData = sdata.tables[table]
+        area_normalized_X = sparse.csr_matrix(adata.X / adata.obs["area"].to_numpy().reshape(-1, 1))
+
+        if method == "replace":
+            adata.X = area_normalized_X
+        elif method == "layer":
+            adata.layers["area_normalized"] = area_normalized_X
+        elif method == "new table":
+            new_table = adata.copy()
+            new_table.X = area_normalized_X
+            sdata.tables["area_normalized"] = new_table
+    return None if inplace else sdata
+
+
+class NeighborsKwargs(TypedDict):
+    """Keyword arguments for the Neighbors function from Scanpy."""
+
+    n_neighbors: int
+    n_pcs: int
+    use_rep: str
+    knn: bool = True
+    method: Literal["umap", "gauss"] = "umap"
+    transformer: str | None | Any = None
+    metric: Literal[
+        "cityblock",
+        "cosine",
+        "euclidean",
+        "l1",
+        "l2",
+        "manhattan",
+        "braycurtis",
+        "canberra",
+        "chebyshev",
+        "correlation",
+        "dice",
+        "hamming",
+        "jaccard",
+        "kulsinski",
+        "mahalanobis",
+        "minkowski",
+        "rogerstanimoto",
+        "russellrao",
+        "seuclidean",
+        "sokalmichener",
+        "sokalsneath",
+        "sqeuclidean",
+        "yule",
+    ] = "euclidean"
+    metric_kwds: Mapping
+    random_state: int | None | Any = 0
+
+
+def neighbors(
+    sdata: sd.SpatialData,
+    table_name: str,
+    layer: str | None = None,
+    vars: Sequence[str] | None = None,
+    key_added: str | None = None,
+    inplace: bool = True,
+    write: bool = True,
+    **sc_neighbors_kwargs: Unpack[NeighborsKwargs],
+) -> sd.SpatialData:
+    """Computes the neighbors of a table in a SpatialData object.
+
+    Parameters
+    ----------
+    sdata
+        _description_
+    table_name
+        _description_
+    layer, optional
+        _description_, by default None
+    vars, optional
+        _description_, by default None
+    key_added, optional
+        _description_, by default None
+    inplace, optional
+        _description_, by default True
+    write, optional
+        _description_, by default True
+
+    Returns
+    -------
+        _description_
+    """
+    q: ad.AnnData = sc.pp.neighbors(
+        adata=_extract_layer_from_sdata(sdata=sdata, vars=vars, table_name=table_name, layer=layer),
+        key_added=key_added,
+        copy=True,
+        **sc_neighbors_kwargs,
+    )
+
+    sdata.tables[table_name].obsp[f"{key_added}_connectivities"] = q.obsp[f"{key_added}_connectivities"]
+    sdata.tables[table_name].obsp[f"{key_added}_distances"] = q.obsp[f"{key_added}_distances"]
+    sdata.tables[table_name].uns[key_added] = q.uns[key_added]
+
+    if write:
+        write_elements(sdata=sdata, elements={"tables": table_name})
     return None if inplace else sdata
--- src/nbl/tl/__init__.py
+++ src/nbl/tl/__init__.py
@@ -1,3 +1,25 @@
-from .basic import aggregate_images_by_labels, filter_obs_names_by_quantile, quantile, regionprops
+from ._utils import _scores
+from .basic import (
+    aggregate_images_by_labels,
+    compute_marker_means,
+    compute_score,
+    diffmap,
+    filter_obs_names_by_quantile,
+    leiden,
+    quantile,
+    regionprops,
+    umap,
+)
 
-__all__ = ["aggregate_images_by_labels", "regionprops", "quantile", "filter_obs_names_by_quantile"]
+__all__ = [
+    "_scores",
+    "aggregate_images_by_labels",
+    "compute_marker_means",
+    "compute_score",
+    "diffmap",
+    "filter_obs_names_by_quantile",
+    "leiden",
+    "quantile",
+    "regionprops",
+    "umap",
+]
