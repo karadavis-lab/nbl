594413469629796aacc993e0db855723eac4d8c1
Sricharan Reddy Varra <sricharanvarra@gmail.com>
2025-05-03 15:55:32 -0700

    🎉 Begin Analysis Project

--- src/nbl/__init__.py
+++ src/nbl/__init__.py
@@ -1,7 +1,15 @@
 from importlib.metadata import version
 
-from . import pl, pp, tl
+from lamindb import settings
+from loguru import logger
 
-__all__ = ["pl", "pp", "tl"]
+from . import ln, pl, pp, tl
+
+__all__ = ["ln", "pl", "pp", "tl"]
 
 __version__ = version("nbl")
+
+
+settings.sync_git_repo = ln.settings.git_repo
+
+logger.add("logs/lamindb.log")
